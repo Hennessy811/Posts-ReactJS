4a9e6eea7a1ec76e59b5dbab39484477244a068e
Kosilov Alex 4917 <Skosilovs@mail.ru>
2021-03-08 04:01:14 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,30 @@ import Posts from './component/Posts'
 import styled from 'styled-components'
 
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  
+	display: flex;
+	justify-content: center;
 `
 const Wrapper = styled.div`
 	max-width: 1200px;
 	display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding-top:2rem
+	flex-direction: column;
+	justify-content: center;
+	padding-top: 2rem;
 `
 
 function App() {
-	const [ users, setUsers ] = React.useState([]) 
+	const [ users, setUsers ] = React.useState([])
 	const [ posts, setPost ] = React.useState([])
 
 	React.useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/posts')
 			.then((res) => res.json())
 			.then((res) => setPost(res))
-      .then(fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((res) => setUsers(res))) 
+			.then(fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json()).then((res) => setUsers(res)))
 	}, [])
 	return (
 		<Container>
-			<Wrapper>
-				{users[0] && posts[0] && <Posts users={users} posts={posts} />} 
-			</Wrapper>
+			<Wrapper>{users[0] && posts[0] && <Posts users={users} posts={posts} />}</Wrapper>
 		</Container>
 	)
 }
