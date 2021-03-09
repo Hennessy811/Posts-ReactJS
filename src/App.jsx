d7fbfc1b8c8378902d2fb5e7d0c569a06cb35034
Kosilov Alex 4917 <Skosilovs@mail.ru>
2021-03-09 03:45:09 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -13,38 +13,38 @@ const Wrapper = styled.div`
 	justify-content: center;
 	padding-top: 2rem;
 `
-const usersArr = []
-const postsArr = [] // если создавать через state, то будут лишние рендеры; это косяк или выносить можно?
+
+function mergeData(posts, users) {
+	const result = posts.map((el) => {
+		return {
+			...el,
+			name: users[el.userId - 1].name,
+			username: users[el.userId - 1].username
+		}
+	})
+	return result
+}
 
 function App() {
-	const [ render, setRender ] = React.useState(false)
-
+	const [ data, setData ] = React.useState([]) // здесь будут храниться оба массива
+	const result = [] // Новый массив, который будет состоять состоит из posts + name и username из массива users
 	React.useEffect(() => {
-		const posts = new Promise((resolve, reject) => { // Как здесь правильно? как правильно сделать два запроса?
+		const posts = new Promise((resolve, reject) => {
+			// Как здесь правильно? как правильно сделать два запроса?
 			resolve(fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json()))
 		})
 		const users = new Promise((resolve, reject) => {
 			resolve(fetch('https://jsonplaceholder.typicode.com/users').then((res) => res.json()))
 		})
 		Promise.all([ posts, users ]).then((data) => {
-			usersArr.push(...data[1])
-			postsArr.push(...data[0])
-			setRender(true)
+			setData(data)
 		})
 	}, [])
-	const result = postsArr.map((el) => { // Новый массив, который состоит из posts + name и username из массива users
-		return {
-			...el,
-			name: usersArr[el.userId - 1].name,
-			username: usersArr[el.userId - 1].username
-		}
-	})
 
-	return (
-		<Container>
-			<Wrapper>{result[0] ? <Posts result={result} /> : <div>Loading...</div>} </Wrapper>
-		</Container>
-	)
+	if (data[0]) {
+		result.push(...mergeData(data[0], data[1]))
+	}
+	return <Container>{<Wrapper>{result[0] ? <Posts result={result} /> : <div>Loading...</div>} </Wrapper>}</Container>
 }
 
 export default App
